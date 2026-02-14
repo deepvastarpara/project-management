39ecf34da768e9d213ef8b115fa418b572e66cd5
deepvastarpara <deepvastarpara7@gmail.com>
2026-02-14 12:38:04 +0530

    fix bug in inngest

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,4 +1,5 @@
 import { Inngest } from "inngest";
+import prisma from "../config/prisma.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "project management" });
@@ -8,11 +9,11 @@ const syneUserCreation = inngest.createFunction(
     { id: "syne-user-from-clerk" },
     { event: "clerk/user.created" },
     async ({ event }) => {
-        const { data } = event;
+        const { data } = event
         await prisma.user.create({
             data: {
                 id: data.id,
-                email: data?.email_addresses[0]?.email_addres,
+                email: data?.email_addresses[0]?.email_address,
                 name: data?.first_name + " " + data?.last_name,
                 image: data?.image_url,
             },
@@ -26,7 +27,7 @@ const syneUserDeletion = inngest.createFunction(
     { id: "delete-user-with-clerk" },
     { event: "clerk/user.deleted" },
     async ({ event }) => {
-        const { data } = event;
+        const { data } = event
         await prisma.user.delete({
             where: {
                 id: data.id,
@@ -40,13 +41,13 @@ const syneUserUpdation = inngest.createFunction(
     { id: "update-user-from-clerk" },
     { event: "clerk/user.updated" },
     async ({ event }) => {
-        const { data } = event;
+        const { data } = event
         await prisma.user.update({
             where: {
                 id: data.id,
             },
             data: {
-                email: data?.email_addresses[0]?.email_addres,
+                email: data?.email_addresses[0]?.email_address,
                 name: data?.first_name + " " + data?.last_name,
                 image: data?.image_url,
             },
